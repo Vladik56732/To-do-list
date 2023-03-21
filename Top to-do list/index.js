7d0270fd32468f4cd57ec30c718811d56a588678
Vladik56732 <125814679+Vladik56732@users.noreply.github.com>
2023-03-21 19:55:43 +0300

    Add files via upload

---
+++ Top to-do list/index.js
@@ -0,0 +1,43 @@
+const toDoItems = document.querySelector('.to-do-items')
+const input = document.querySelector('#input')
+const trashIcon = document.querySelector('#trash')
+
+input.addEventListener('keydown', event => {
+    if (event.key === 'Enter') {
+        addItem()
+    }
+})
+
+function addItem() {
+    if (input.value.trim() != '') {
+        const divParent = document.createElement('div')
+        const divChild = document.createElement('div')
+        const checkIcon = document.createElement('i')
+        const trashIcon = document.createElement('i')
+
+        divParent.className = 'item'
+        divParent.innerHTML = `<div>${input.value}</div>`
+
+        checkIcon.className = 'fa fa-plus-circle'
+        checkIcon.style.color = 'fa fa-trash'
+        checkIcon.addEventListener('click', () => {
+            checkIcon.style.color = 'limegreen'
+        })
+
+        divChild.appendChild(checkIcon)
+
+        trashIcon.className = 'fa fa-trash'
+        trashIcon.style.color = 'darkgray'
+        trashIcon.addEventListener('click', () => {
+            divParent.remove()
+        })
+
+        divChild.appendChild(trashIcon)
+
+        divParent.appendChild(divChild)
+
+        toDoItems.appendChild(divParent)
+
+        input.value = ''
+    }
+}
